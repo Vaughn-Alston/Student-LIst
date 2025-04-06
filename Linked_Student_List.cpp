99da78e88a37df857ca75b33c6f47d2cea0149c8
Vaughn-Alston <vaughn2code@gmail.com>
2025-04-06 15:51:53 -0700

    Creating the Delete Node Function and adding comments

--- Linked_Student_List.cpp
+++ Linked_Student_List.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// A linked list is a numer of values being connected by pointers
+
 
 //Creating the Node Class
 class Node
@@ -26,29 +28,47 @@ class LinkedList
     //constructor to initalize a empty list
     LinkedList()
     {
-        head = nullptr;
+        head = nullptr; // ensure the head is empty 
     }
 
     void setHead(Node*node)
     {
-        head = node;
+        head = node; // Head is Literally Node
     }
-    void display()
+    void display() 
     {
-        Node* curr = head;
+        Node* curr = head; // Node pointer curr = head;
 
-        while(curr != nullptr)
+        while(curr != nullptr) // while not at the end of the list
         {
-            cout << curr-> data << " ";
-            curr = curr->next;
+            cout << curr-> data << " "; //curr is at the head this line will read the int data inside
+            curr = curr->next; // this line will curr = curr -> will read the data to the next of curr
         }
         cout << "Null" << endl;
     }// end of display function
 
-    
+    void DeleteNode(int m)
+    {
+        Node* dptr = nullptr; 
+        Node* temp = head; //temp node point to first value in list.
+        Node* curr = head; //curr node also points at the first value in the node.
 
+        while(curr != nullptr && curr->data != m) // Loop while curr not at the end of list and curr not equal to data.
+        {
+            temp = curr; //temp node and curr are at the same position
+            curr = curr-> next; //curr = curr point to the next node
 
-    
+        }
+        if (curr = nullptr) //if we reach the  end of the list
+        {
+            cout << m << "not in list\n"; 
+        }
+
+        //if we exit the loop
+        delete dptr;
+        cout << "The Value " << m << "was deleted";
+    }// End of Delete Node Function 
+  
 
 
 
