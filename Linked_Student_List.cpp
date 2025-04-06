915058ea55955dce793377710915547d557e4194
Vaughn Alston <vaughn2code@gmail.com>
2025-04-06 13:09:06 -0700

    Creating the Node Class

--- Linked_Student_List.cpp
+++ Linked_Student_List.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+
+//Creating the Node Class
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    //constructor to initialize node with data
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+}; // End of Node Class
